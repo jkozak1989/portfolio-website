b200398bc282d7c473bc87ff685d72f5d9cd5aeb
jkozak1989 <98820821+jkozak1989@users.noreply.github.com>
2024-09-02 12:11:14 +0100

    add content to Technologies page

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -1,15 +1,31 @@
 import { useContext } from 'react';
 import { PageContext } from '../../PageContext';
 import './Technologies.css';
+import { Card } from '../Card/Card';
 
 export const Technologies = () => {
   const { page } = useContext(PageContext);
   return (
-    <section
-      id='technologies'
-      className={`content-wrapper${page == 'Technologies' ? ' content-wrapper-visible' : ''}`}
-    >
-      Technologies
+    <section className={`content-wrapper${page == 'Technologies' ? ' content-wrapper-visible' : ''} technologies`}>
+      <div className='technologies-title'>Tech Stack</div>
+      <div className='technologies-list'>
+        <Card>HTML</Card>
+        <Card>CSS</Card>
+        <Card>JavaScript</Card>
+        <Card>TypeScript</Card>
+        <Card>React</Card>
+        <Card>C#</Card>
+        <Card>.NET</Card>
+        <Card>SQL</Card>
+      </div>
+      <div className='technologies-title'>Learning Journey</div>
+      <div className='technologies-list'>
+        <Card>React Native</Card>
+        <Card>Node</Card>
+        <Card>Unity</Card>
+        <Card>Solidity</Card>
+        <Card>Web3</Card>
+      </div>
     </section>
   );
 };
